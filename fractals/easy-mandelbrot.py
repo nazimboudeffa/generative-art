74bc927e7ce992ef4560e37db64745f1fe3850c6
Nazim Boudeffa <boudeffa@gmail.com>
2021-11-09 16:38:02 +0100

    Update easy-mandelbrot.py

--- fractals/easy-mandelbrot.py
+++ fractals/easy-mandelbrot.py
@@ -1,21 +1,21 @@
 from PIL import Image
 
-xa = -2.0
-xb = 2.0
-ya = -2.0
-yb = 2.0
+x1 = -2.0
+x2 = 2.0
+y1 = -2.0
+y2 = 2.0
 
 n = 100
-imgx = 1080
-imgy = 1080
+w = 1080
+h = 1080
 
-image = Image.new("RGB", (imgx, imgy))
+image = Image.new("RGB", (w, h))
 
-for x in range(imgx):
-    for y in range(imgy):
+for x in range(w):
+    for y in range(h):
         
-        a = x * (xb - xa) / imgx  + xa
-        b = y * (yb - ya) / imgy  + ya
+        a = x * (x2 - x1) / w  + x1
+        b = y * (y2 - y1) / h  + y1
         
         c = a + 1j * b
         z = c
